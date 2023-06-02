c1c0ddfc9a3525a31289307eb1f9515b7ca2c304
Mehmet Anıl <67124843+mehmetanil10@users.noreply.github.com>
2023-06-02 17:38:33 +0300

    Add files via upload

---
+++ Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication9.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
---
+++ Models/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication9.Models
+{
+    public class Skill
+    {
+        public int SkillId { get; set; }
+        public string SkillEx { get; set; }
+        public byte SkillPercent { get; set; }
+
+    }
+}
