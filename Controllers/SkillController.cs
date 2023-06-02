578451572a7437fa1811ecad87493d1631258337
Mehmet Anıl <67124843+mehmetanil10@users.noreply.github.com>
2023-06-02 17:50:16 +0300

    Add files via upload

---
+++ Controllers/SkillController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class SkillController : Controller
+    {
+        // GET: Skill
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            List<Skill> skillList=db.Skills.ToList();
+            return View(skillList);
+        }
+    }
+}
